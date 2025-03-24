21c45aaf4799fbd45252e1a874fa6ab20576c016
Lucas Stopinski da Silva <160746304+LucasStop@users.noreply.github.com>
2025-03-23 22:57:35 -0300

    feat: refatorar modelos e rotas para gerenciamento de comentários e receitas

---
+++ backend/routes/comment_recipe.routes.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+const commentController = require("../controllers/comment_recipe.controller");
+
+router.post("/:recipeId", commentController.addComment);
+router.delete("/:id", commentController.deleteComment);
