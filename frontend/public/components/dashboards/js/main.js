f3bd6c35b5fc5ba098d58f1c2ebe99f86f47f826
jvecodev <jvitor.oliveira1803@gmail.com>
2025-03-24 19:36:04 -0300

    feat(Daashboards):finalizado dashboards para pagina de admin

--- frontend/public/components/dashboards/js/main.js
+++
@@ -1,9 +0,0 @@
-import { CakeChart } from './components/cakeChart.js';
-import { UserChart } from './components/userChart.js';
-import { OrderValueChart } from './components/orderValueChart.js';
-
-document.addEventListener('DOMContentLoaded', function() {
-    CakeChart();
-    UserChart();
-    OrderValueChart();
-});
