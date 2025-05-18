20e490134548e09ee84874ea619e97958b6490df
LucasStop <160746304+LucasStop@users.noreply.github.com>
2025-05-18 12:10:33 -0300

    feat: Adicionar autenticação nas rotas de endereço, categoria e produto

--- backend/routes/category.routes.js
+++ backend/routes/category.routes.js
@@ -3,16 +3,14 @@ const router = express.Router();
 const { authenticate } = require('../middlewares/auth.middleware');
 const categoryController = require('../controllers/category.controller');
 
+// Rotas públicas
 router.get('/', categoryController.findAll);
 router.get('/slug/:slug', categoryController.findBySlug);
 router.get('/:id', categoryController.findOne);
-router.post('/', categoryController.create);
-router.put('/:id', categoryController.update);
-router.delete('/:id', categoryController.delete);
 
 // Rotas que precisam de autenticação
-// router.post('/', authenticate, categoryController.create);
-// router.put('/:id', authenticate, categoryController.update);
-// router.delete('/:id', authenticate, categoryController.delete);
+router.post('/', authenticate, categoryController.create);
+router.put('/:id', authenticate, categoryController.update);
+router.delete('/:id', authenticate, categoryController.delete);
 
 module.exports = router;
--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/product.controller');
+const { authenticate } = require('../middlewares/auth.middleware');
 
+// Rotas públicas
 router.get('/', productController.findAll);
 router.get('/image/:id', productController.getImage); 
 router.get('/slug/:slug', productController.findBySlug);
 router.get('/category/:categoryId', productController.findByCategory);
 router.get('/:id', productController.findOne);
-router.post('/', productController.create);
-router.put('/:id', productController.update);
-router.delete('/:id', productController.delete);
+
+// Rotas protegidas que requerem autenticação
+router.post('/', authenticate, productController.create);
+router.put('/:id', authenticate, productController.update);
+router.delete('/:id', authenticate, productController.delete);
 
 module.exports = router;
